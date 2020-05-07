99cd88464d3ca8268c1b59c0b7e508cf718d7eea
Роман <elfiroma@gmail.com>
2020-05-07 10:28:16 +0300

    remove data

--- TasksWithFirebase/Controller/TasksViewController.swift
+++ TasksWithFirebase/Controller/TasksViewController.swift
@@ -29,11 +29,37 @@ class TasksViewController: UIViewController {
         ref = Database.database().reference(withPath: "users").child(String(user.uid)).child("tasks")
     }
     
+    // Создаем наблюдателя
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        ref.observe(.value) { [weak self] (snapshot) in
+            
+            var _tasks = Array<Task>()
+            for item in snapshot.children {
+                //Получаем task
+                let task = Task(snapshot: item as! DataSnapshot)
+                _tasks.append(task)
+            }
+            
+            self?.tasks = _tasks
+            self?.tableView.reloadData()
+        }
+    }
+    
+    //Удаляем наблюдателя по выходу
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        ref.removeAllObservers()
+    }
+    
+    
     //MARK: - Action
     @IBAction func addTapped(_ sender: UIBarButtonItem) {
         
         let alertController = UIAlertController(title: "Новая задача", message: "Добавьте задачу", preferredStyle: .alert)
         alertController.addTextField()
+        
         let save = UIAlertAction(title: "Сохранить", style: .default) { [weak self] _ in
             
             guard let textField = alertController.textFields?.first, textField.text != "" else { return }
@@ -70,18 +96,51 @@ class TasksViewController: UIViewController {
 extension TasksViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return tasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        cell.textLabel?.text = "\(indexPath.row)"
-        
         cell.backgroundColor = .clear
+        cell.textLabel?.textColor = .white
+        
+        let task = tasks[indexPath.row]
+        let taskTitle = task.title
+        cell.textLabel?.text = taskTitle
+        
+        let isCompleted = task.completed
+        toggleCompetion(cell, isCompleted: isCompleted)
         
         return cell
     }
     
+    // Editing
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let task = tasks[indexPath.row]
+            // Удалить объект
+            task.ref?.removeValue()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) else { return }
+        let task = tasks[indexPath.row]
+        let isCompleted = !task.completed
+        
+        toggleCompetion(cell, isCompleted: isCompleted)
+        //Передаем изменения в базу данных
+        task.ref?.updateChildValues(["completed": isCompleted])
+    }
+        
+    // Отрисовать галочку
+    func toggleCompetion(_ cell: UITableViewCell, isCompleted: Bool) {
+        cell.accessoryType = isCompleted ? .checkmark : .none
+    }
     
 }
